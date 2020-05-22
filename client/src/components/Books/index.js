7c611ef02fa71b8ac470ed3eca52c731d1336ddf
Skyler Tosh <skylerjtosh@gmail.com>
2020-05-22 12:40:18 -0700

    Added constructor and the prop handleSearch

--- client/src/components/Books/index.js
+++ client/src/components/Books/index.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import SearchArea from '../SearchArea';
 
+
 class Books extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            books: [],
+            searchField: ''
+        }
+    }
+
+    handleSearch = (e) => {
+        console.log(e.target.value);
+        this.setState ({ searchField: e.target.value })
+    }
+
     render () {
         return (
             <div>
-                <SearchArea />
+                <SearchArea handleSearch={this.handleSearch}/>
             </div>
         );
     }
