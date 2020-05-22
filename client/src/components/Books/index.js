0aa876bbd3c5f095c1624fff181565f3311bc3e1
Skyler Tosh <skylerjtosh@gmail.com>
2020-05-22 12:31:18 -0700

    Adding SearchArea component

--- client/src/components/Books/index.js
+++ client/src/components/Books/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import SearchArea from '../SearchArea';
 
 class Books extends Component {
     render () {
         return (
             <div>
-
+                <SearchArea />
             </div>
         );
     }
